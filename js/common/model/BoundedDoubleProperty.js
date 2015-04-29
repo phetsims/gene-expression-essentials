e3e9a2df0ba2acd2f6b152e5a1f93ceda70a02f4
safi <msafiyullah@gmail.com>
2015-04-29 21:22:51 +0530

    common model  - BoundedDoubleProperty added

---
+++ js/common/model/BoundedDoubleProperty.js
@@ -0,0 +1,43 @@
+// Copyright 2002-2014, University of Colorado Boulder
+
+/**
+ * @author Sam Reid (PhET Interactive Simulations)
+ * @author Mohamed Safi
+ */
+
+define( function( require ) {
+  'use strict';
+
+  //modules
+  var inherit = require( 'PHET_CORE/inherit' );
+  var Property = require( 'AXON/Property' );
+  var Util = require( 'DOT/Util' );
+  var DoubleRange = require( 'GENE_EXPRESSION_BASICS/common/model/util/DoubleRange' );
+
+  /**
+   * Constraint value between min and max value
+   * @param  {number} value
+   * @param {number} minValue
+   * @param  {number} maxValue
+   * @constructor
+   */
+
+  function BoundedDoubleProperty( value, minValue, maxValue ) {
+    Property.call( this, value );
+    this.bounds = new Property( new DoubleRange( minValue, maxValue ) );
+  }
+
+  return inherit( Property, BoundedDoubleProperty, {
+
+
+    /**
+     *
+     * @param {number} value
+     */
+    set: function( value ) {
+      var boundedValue = Util.clamp( value, this.bounds.get().getMin(), this.bounds.get().getMax() );
+      Property.prototype.set( boundedValue );
+    }
+
+  } );
+} );
