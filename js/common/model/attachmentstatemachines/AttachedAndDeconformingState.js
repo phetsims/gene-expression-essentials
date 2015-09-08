37ff8897124e1709ac47ff0b4e5a57f0f97481ba
Ashraf <ashrafthahir@gmail.com>
2015-09-08 22:45:44 +0530

    AttachedAndTranscribing State uses EqualsEpislon instead of  Equals

--- js/common/model/attachmentstatemachines/AttachedAndDeconformingState.js
+++ js/common/model/attachmentstatemachines/AttachedAndDeconformingState.js
@@ -12,7 +12,7 @@ define( function( require ) {
 
   // modules
   var inherit = require( 'PHET_CORE/inherit' );
-   var AttachmentState = require( 'GENE_EXPRESSION_BASICS/common/model/attachmentstatemachines/AttachmentState' );
+  var AttachmentState = require( 'GENE_EXPRESSION_BASICS/common/model/attachmentstatemachines/AttachmentState' );
   var BeingRecycledState = require( 'GENE_EXPRESSION_BASICS/common/model/attachmentstatemachines/BeingRecycledState' );
 
 
@@ -23,81 +23,80 @@ define( function( require ) {
    * @param {RnaPolymeraseAttachmentStateMachine} rnaPolymeraseAttachmentStateMachine
    * @constructor
    */
-  function AttachedAndDeconformingState(rnaPolymeraseAttachmentStateMachine) {
+  function AttachedAndDeconformingState( rnaPolymeraseAttachmentStateMachine ) {
     AttachmentState.call( this );
     this.rnaPolymeraseAttachmentStateMachine = rnaPolymeraseAttachmentStateMachine;
     this.conformationalChangeAmount = 0;
   }
 
 
-  return inherit( AttachmentState, AttachedAndDeconformingState,{
-    /**
-     * @param  {AttachmentStateMachine} asm
-     * @param {number} dt
-     */
+  return inherit( AttachmentState, AttachedAndDeconformingState, {
+      /**
+       * @param  {AttachmentStateMachine} asm
+       * @param {number} dt
+       */
 
-    stepInTime: function( asm, dt ) {
-      var biomolecule = this.rnaPolymeraseAttachmentStateMachine.biomolecule;
+      stepInTime: function( asm, dt ) {
+        var biomolecule = this.rnaPolymeraseAttachmentStateMachine.biomolecule;
 
-      // Verify that state is consistent.
-      assert && assert( asm.attachmentSite !== null);
-      assert && assert(asm.attachmentSite.attachedOrAttachingMolecule === biomolecule);
+        // Verify that state is consistent.
+        assert && assert( asm.attachmentSite !== null );
+        assert && assert( asm.attachmentSite.attachedOrAttachingMolecule === biomolecule );
 
-      var dnaStrandSeparation = this.rnaPolymeraseAttachmentStateMachine.dnaStrandSeparation;
-      var rnaPolymerase = this.rnaPolymeraseAttachmentStateMachine.rnaPolymerase;
-      var attachmentSite = this.rnaPolymeraseAttachmentStateMachine.attachmentSite;
-      var recycleMode = this.rnaPolymeraseAttachmentStateMachine.recycleMode;
-      var recycleReturnZones = this.rnaPolymeraseAttachmentStateMachine.recycleReturnZones;
-      var attachedAndWanderingState = this.rnaPolymeraseAttachmentStateMachine.attachedAndWanderingState;
+        var dnaStrandSeparation = this.rnaPolymeraseAttachmentStateMachine.dnaStrandSeparation;
+        var rnaPolymerase = this.rnaPolymeraseAttachmentStateMachine.rnaPolymerase;
+        var attachmentSite = this.rnaPolymeraseAttachmentStateMachine.attachmentSite;
+        var recycleMode = this.rnaPolymeraseAttachmentStateMachine.recycleMode;
+        var recycleReturnZones = this.rnaPolymeraseAttachmentStateMachine.recycleReturnZones;
+        var attachedAndWanderingState = this.rnaPolymeraseAttachmentStateMachine.attachedAndWanderingState;
 
 
-      this.conformationalChangeAmount = Math.max( this.conformationalChangeAmount -
-                                                  CONFORMATIONAL_CHANGE_RATE * dt, 0 );
-      biomolecule.changeConformation( this.conformationalChangeAmount );
-      dnaStrandSeparation.setProportionOfTargetAmount( this.conformationalChangeAmount );
-      if ( this.conformationalChangeAmount === 0 ) {
+        this.conformationalChangeAmount = Math.max( this.conformationalChangeAmount -
+                                                    CONFORMATIONAL_CHANGE_RATE * dt, 0 );
+        biomolecule.changeConformation( this.conformationalChangeAmount );
+        dnaStrandSeparation.setProportionOfTargetAmount( this.conformationalChangeAmount );
+        if ( this.conformationalChangeAmount === 0 ) {
 
-        // Remove the DNA separator, which makes the DNA close back up.
-        rnaPolymerase.getModel().getDnaMolecule().removeSeparation( dnaStrandSeparation );
+          // Remove the DNA separator, which makes the DNA close back up.
+          rnaPolymerase.getModel().getDnaMolecule().removeSeparation( dnaStrandSeparation );
 
-        // Update externally visible state indication.
-        asm.biomolecule.attachedToDna = false;
+          // Update externally visible state indication.
+          asm.biomolecule.attachedToDna = false;
 
-        // Make sure that we enter the correct initial state upon the
-        // next attachment.
-        this.rnaPolymeraseAttachmentStateMachine.attachedState = attachedAndWanderingState;
+          // Make sure that we enter the correct initial state upon the
+          // next attachment.
+          this.rnaPolymeraseAttachmentStateMachine.attachedState = attachedAndWanderingState;
 
-        // Detach from the DNA.
-        attachmentSite.attachedOrAttachingMolecule = null;
-        attachmentSite = null;
-        if ( recycleMode ) {
-          this.rnaPolymeraseAttachmentStateMachine.setState(
-            new BeingRecycledState( this.rnaPolymeraseAttachmentStateMachine, recycleReturnZones ) );
+          // Detach from the DNA.
+          attachmentSite.attachedOrAttachingMolecule = null;
+          attachmentSite = null;
+          if ( recycleMode ) {
+            this.rnaPolymeraseAttachmentStateMachine.setState(
+              new BeingRecycledState( this.rnaPolymeraseAttachmentStateMachine, recycleReturnZones ) );
+          }
+          else {
+            this.rnaPolymeraseAttachmentStateMachine.forceImmediateUnattachedButUnavailable();
+          }
         }
-        else {
-          this.rnaPolymeraseAttachmentStateMachine.forceImmediateUnattachedButUnavailable();
-        }
+      },
+
+      /**
+       * @Override
+       * @param {AttachmentStateMachine} asm
+       */
+      entered: function( asm ) {
+        // Prevent user interaction.
+        asm.biomolecule.movableByUser = false;
+        this.conformationalChangeAmount = 1;
       }
+
     },
 
-    /**
-     * @Override
-     * @param {AttachmentStateMachine} asm
-     */
-    entered: function( asm ) {
+    {
 
-      // Prevent user interaction.
-      asm.biomolecule.movableByUser =  false;
-      this.conformationalChangeAmount = 1;
+      CONFORMATIONAL_CHANGE_RATE: CONFORMATIONAL_CHANGE_RATE
+
     }
-
-  },
-
-  {
-
-    CONFORMATIONAL_CHANGE_RATE:CONFORMATIONAL_CHANGE_RATE
-
-  }
   );
 
 } );
