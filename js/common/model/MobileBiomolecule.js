469d8e16b5e5151afd8eb33ad13f6cd88b76d58a
Jonathan Olson <olsonsjc@gmail.com>
2017-03-08 14:44:59 -0700

    Lint fix

--- js/common/model/MobileBiomolecule.js
+++ js/common/model/MobileBiomolecule.js
@@ -17,7 +17,6 @@ define( function( require ) {
   var Color = require( 'SCENERY/util/Color' );
   var Vector2 = require( 'DOT/Vector2' );
   var Vector3 = require( 'DOT/Vector3' );
-  var Matrix3 = require( 'DOT/Matrix3' );
   var Property = require( 'AXON/Property' );
   var ShapeChangingModelElement = require( 'GENE_EXPRESSION_ESSENTIALS/common/model/ShapeChangingModelElement' );
   var MotionBounds = require( 'GENE_EXPRESSION_ESSENTIALS/common/model/motion-strategies/MotionBounds' );
