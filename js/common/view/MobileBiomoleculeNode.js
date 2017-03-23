a35dc576971baacc9e02c61686f83aa148d3e8f3
aadish <aadishgupta7791@gmail.com>
2017-03-23 15:23:01 -0600

    performance boost remove transformation of Messenger RNA

--- js/common/view/MobileBiomoleculeNode.js
+++ js/common/view/MobileBiomoleculeNode.js
@@ -14,16 +14,12 @@ define( function( require ) {
   var geneExpressionEssentials = require( 'GENE_EXPRESSION_ESSENTIALS/geneExpressionEssentials' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Node = require( 'SCENERY/nodes/Node' );
-  var Matrix3 = require( 'DOT/Matrix3' );
-  var Bounds2 = require( 'DOT/Bounds2' );
   var Path = require( 'SCENERY/nodes/Path' );
   var Shape = require( 'KITE/Shape' );
   var Color = require( 'SCENERY/util/Color' );
   var GradientUtil = require( 'GENE_EXPRESSION_ESSENTIALS/common/util/GradientUtil' );
   var RnaPolymerase = require( 'GENE_EXPRESSION_ESSENTIALS/common/model/RnaPolymerase' );
   var BiomoleculeDragHandler = require( 'GENE_EXPRESSION_ESSENTIALS/common/view/BiomoleculeDragHandler' );
-  var MessengerRna = require( 'GENE_EXPRESSION_ESSENTIALS/common/model/MessengerRna' );
-  var MessengerRnaFragment = require( 'GENE_EXPRESSION_ESSENTIALS/common/model/MessengerRnaFragment' );
 
 
   /**
@@ -47,36 +43,28 @@ define( function( require ) {
 
     function handleShapeChanged( shape ) {
       // Create a shape that excludes any offset.
-      var centeredShape = self.centeredShape( shape, mobileBiomolecule, mvt );
+      var centeredShape = self.centeredShape( shape, mvt );
       path.setShape( centeredShape );
       // Account for the offset.
       var offset = mvt.modelToViewPosition( mobileBiomolecule.getPosition() );
-
-      if ( mobileBiomolecule instanceof MessengerRna || mobileBiomolecule instanceof MessengerRnaFragment ) {
-        path.x = offset.x;
-        path.y = offset.y;
-      }
-      else {
-        path.centerX = offset.x;
-        path.centerY = offset.y;
-      }
 
       // For shapes with just one point, the Java Version of GeneralPath's "Bounds" returns a width of
       // zero and height of 1 but kite's shape bounds returns infinity in such cases. Since the MessengerRna starts with
       // a single point, the Gradient fill code fails as the bounds of the shape at that point is infinity.
       // So the following check is added before calling createGradientPaint - Ashraf
-
-      // Set the gradient paint.
       if ( _.isFinite( centeredShape.bounds.centerX ) ) {
+        path.centerX = offset.x;
+        path.centerY = offset.y;
         path.fill = GradientUtil.createGradientPaint( centeredShape, mobileBiomolecule.colorProperty.get() );
       }
+
     }
 
     // Update the shape whenever it changes.
     mobileBiomolecule.shapeProperty.link( handleShapeChanged );
 
     function handleColorChanged( color ) {
-      var moleculeShape = self.centeredShape( mobileBiomolecule.getShape(), mobileBiomolecule, mvt );
+      var moleculeShape = self.centeredShape( mobileBiomolecule.getShape(), mvt );
 
       //see the comment above on gradientPaint
       if ( _.isFinite( moleculeShape.bounds.centerX ) ) {
@@ -163,42 +151,20 @@ define( function( require ) {
      */
     getPathByMobileBioMoleculeType: function( mobileBiomolecule, options ) {
 
-      // override the computeShapeBounds based on Molecule  to optimize performance - Ashraf TODO verify
       var path = new Path( new Shape(), options );
-      if ( mobileBiomolecule instanceof  MessengerRna ) {
-        var emptyBounds = new Bounds2( 0, 0, 0, 0 );
-        var emptyComputeShapeBounds = function() { return emptyBounds; };
-        path.computeShapeBounds = emptyComputeShapeBounds;
-      }
-      else {
-        path.boundsMethod = 'unstroked';
-      }
-
+      path.boundsMethod = 'unstroked';
       return path;
     },
 
     /**
      * Get a shape that is positioned such that its center is at point (0, 0).
      *
-     * The Java version of the code has this method of MobileBioMoleculeNode itself. Now finding the center of the shape
-     * is encapsulated into individual BioMolecules so that composite shapes can apply their special cases.
-     * Ex ribosome - Modified by Ashraf
-     *
      * @param shape
      * @param {ModelViewTransform2} mvt
      */
-    centeredShape: function( shape, mobileBiomolecule, mvt ) {
+    centeredShape: function( shape, mvt ) {
       var viewShape = mvt.modelToViewShape( shape );
-      var shapeBounds = viewShape.bounds;
-      var xOffset = shapeBounds.getCenterX();
-      var yOffset = shapeBounds.getCenterY();
-      var transform = Matrix3.translation( -xOffset, -yOffset );
-      if ( mobileBiomolecule instanceof MessengerRna || mobileBiomolecule instanceof MessengerRnaFragment ) {
-        return viewShape.transformed( transform );
-      }
-      else {
-        return viewShape;
-      }
+      return viewShape;
     }
   } );
 } );
