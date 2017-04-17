dbe1fd2a63d2430d747f111edfc2a8f52304d5a4
aadish <aadishgupta7791@gmail.com>
2017-04-17 12:11:49 -0600

    documenting map.js

--- js/common/util/Map.js
+++ js/common/util/Map.js
@@ -29,6 +29,12 @@ define( function( require ) {
   geneExpressionEssentials.register( 'Map', Map );
 
   return inherit( Object, Map, {
+    /**
+     * Adds the key and value pair to the hash map. If the key already exists it is updated by new value.
+     * @param {Object} key
+     * @param {Object} value
+     * @public
+     */
     put: function( key, value ) {
       var index = this.keys.indexOf( key );
       if ( index === -1 ) {
@@ -40,37 +46,72 @@ define( function( require ) {
       }
     },
 
+    /**
+     * Returns the value associated with the key
+     * @param {Object} key
+     * @returns {Object}
+     * @public
+     */
     get: function( key ) {
       return this.values[ this.keys.indexOf( key ) ];
     },
 
+    /**
+     * Checks if the key exists in the hash map
+     * @param {Object} key
+     * @returns {boolean}
+     * @public
+     */
     contains: function( key ) {
       return this.keys.indexOf( key ) !== -1;
     },
 
+    /**
+     * Removes the key and associated value from the hash map
+     * @param {Object} key
+     * @public
+     */
     remove: function( key ) {
       var index = this.keys.indexOf( key );
       this.values.splice( index, 1 );
       this.keys.splice( index, 1 );
     },
 
+    /**
+     * Return the array of Keys
+     * @returns {Array.<Object>}
+     * @public
+     */
     keySet: function() {
       return this.keys;
     },
+
+    /**
+     * Compares map object for equality
+     * @param {Map} obj
+     * @returns {boolean}
+     * @public
+     */
     equals: function( obj ) {
       return _.isEqual( this, obj );
     },
+
+    /**
+     * Reset the hash map
+     * @public
+     */
     clear: function() {
       this.keys = [];
       this.values = [];
     },
 
+    /**
+     * Check if the hash map is empty or not
+     * @returns {boolean}
+     * @public
+     */
     isEmpty: function() {
-      if ( this.keys.length === 0 && this.values.length === 0 ) {
-        return true;
-      }
-      return false;
+      return this.keys.length === 0 && this.values.length === 0;
     }
   } );
-
 } );
