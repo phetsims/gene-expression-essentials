e81d1389f242f26b397d61ce07e70f16790cc1a3
aadish <aadishgupta7791@gmail.com>
2017-04-28 12:36:37 -0600

    reduce number of stroke calls

--- js/common/view/DnaMoleculeCanvasNode.js
+++ js/common/view/DnaMoleculeCanvasNode.js
@@ -53,63 +53,66 @@ define( function( require ) {
     /**
      * Draws the strand segments
      * @param {CanvasRenderingContext2D} context
-     * @param {Array.<Vector2>} strandSegment
+     * @param {Array} strandSegmentArray
      * @param {Color} strokeColor
      * @private
      */
-    drawCurve: function( context, strandSegment, strokeColor ) {
-      var strandSegmentLength = strandSegment.length;
+    drawCurve: function( context, strandSegmentArray, strokeColor ) {
       context.beginPath();
-      context.moveTo( strandSegment[ 0 ].x, strandSegment[ 0 ].y );
-      if ( strandSegmentLength === 1 || strandSegmentLength === 2 ) {
+      for ( var i = 0; i < strandSegmentArray.length; i++ ) {
+        var strandSegment = strandSegmentArray[ i ];
+        var strandSegmentLength = strandSegment.length;
+        context.moveTo( strandSegment[ 0 ].x, strandSegment[ 0 ].y );
+        if ( strandSegmentLength === 1 || strandSegmentLength === 2 ) {
 
-        // Can't really create a curve from this, so draw a straight line to the end point and call it good.
-        context.lineTo(
+          // Can't really create a curve from this, so draw a straight line to the end point and call it good.
+          context.lineTo(
+            strandSegment[ strandSegmentLength - 1 ].x,
+            strandSegment[ strandSegmentLength - 1 ].y
+          );
+          return;
+        }
+
+        // Create the first curved segment.
+        var cp1 = ShapeUtils.extrapolateControlPoint( strandSegment[ 2 ], strandSegment[ 1 ], strandSegment[ 0 ] );
+        context.quadraticCurveTo(
+          cp1.x,
+          cp1.y,
+          strandSegment[ 1 ].x,
+          strandSegment[ 1 ].y
+        );
+
+        // Create the middle segments.
+        for ( var j = 1; j < strandSegmentLength - 2; j++ ) {
+          var segmentStartPoint = strandSegment[ j ];
+          var segmentEndPoint = strandSegment[ j + 1 ];
+          var previousPoint = strandSegment[ j - 1 ];
+          var nextPoint = strandSegment[ ( j + 2 ) ];
+          var controlPoint1 = ShapeUtils.extrapolateControlPoint( previousPoint, segmentStartPoint, segmentEndPoint );
+          var controlPoint2 = ShapeUtils.extrapolateControlPoint( nextPoint, segmentEndPoint, segmentStartPoint );
+          context.bezierCurveTo(
+            controlPoint1.x,
+            controlPoint1.y,
+            controlPoint2.x,
+            controlPoint2.y,
+            segmentEndPoint.x,
+            segmentEndPoint.y
+          );
+        }
+
+        // Create the final curved segment.
+        cp1 = ShapeUtils.extrapolateControlPoint( strandSegment[ strandSegmentLength - 3 ],
+          strandSegment[ strandSegmentLength - 2 ],
+          strandSegment[ strandSegmentLength - 1 ]
+        );
+
+        context.quadraticCurveTo(
+          cp1.x,
+          cp1.y,
           strandSegment[ strandSegmentLength - 1 ].x,
           strandSegment[ strandSegmentLength - 1 ].y
         );
-        return;
       }
-
-      // Create the first curved segment.
-      var cp1 = ShapeUtils.extrapolateControlPoint( strandSegment[ 2 ], strandSegment[ 1 ], strandSegment[ 0 ] );
-      context.quadraticCurveTo(
-        cp1.x,
-        cp1.y,
-        strandSegment[ 1 ].x,
-        strandSegment[ 1 ].y
-      );
-
-      // Create the middle segments.
-      for ( var j = 1; j < strandSegmentLength - 2; j++ ) {
-        var segmentStartPoint = strandSegment[ j ];
-        var segmentEndPoint = strandSegment[ j + 1 ];
-        var previousPoint = strandSegment[ j - 1 ];
-        var nextPoint = strandSegment[ ( j + 2 ) ];
-        var controlPoint1 = ShapeUtils.extrapolateControlPoint( previousPoint, segmentStartPoint, segmentEndPoint );
-        var controlPoint2 = ShapeUtils.extrapolateControlPoint( nextPoint, segmentEndPoint, segmentStartPoint );
-        context.bezierCurveTo(
-          controlPoint1.x,
-          controlPoint1.y,
-          controlPoint2.x,
-          controlPoint2.y,
-          segmentEndPoint.x,
-          segmentEndPoint.y
-        );
-      }
-
-      // Create the final curved segment.
-      cp1 = ShapeUtils.extrapolateControlPoint( strandSegment[ strandSegmentLength - 3 ],
-        strandSegment[ strandSegmentLength - 2 ],
-        strandSegment[ strandSegmentLength - 1 ]
-      );
-
-      context.quadraticCurveTo(
-        cp1.x,
-        cp1.y,
-        strandSegment[ strandSegmentLength - 1 ].x,
-        strandSegment[ strandSegmentLength - 1 ].y
-      );
       context.strokeStyle = strokeColor.computeCSS();
       context.lineWidth = this.backboneStrokeWidth;
       context.stroke();
@@ -121,17 +124,26 @@ define( function( require ) {
      * @param {CanvasRenderingContext2D} context
      */
     paintCanvas: function( context ) {
+      var strand1ArrayBehind = [];
+      var strand2ArrayBehind = [];
+      var strand1ArrayFront = [];
+      var strand2ArrayFront = [];
       for ( var i = 0; i < this.model.strand1Segments.length; i++ ) {
         var strand1Segment = this.model.strand1Segments[ i ];
         var strand2Segment = this.model.strand2Segments[ i ];
 
         if ( i % 2 === 0 ) {
-          this.drawCurve( context, strand2Segment, STRAND_2_COLOR );
+          strand2ArrayBehind.push( strand2Segment );
+          strand1ArrayFront.push( strand1Segment );
         }
         else {
-          this.drawCurve( context, strand1Segment, STRAND_1_COLOR );
+          strand1ArrayBehind.push( strand1Segment );
+          strand2ArrayFront.push( strand2Segment );
         }
       }
+
+      this.drawCurve( context, strand1ArrayBehind, STRAND_1_COLOR );
+      this.drawCurve( context, strand2ArrayBehind, STRAND_2_COLOR );
       context.beginPath();
 
       for ( i = 0; i < this.model.basePairs.length; i++ ) {
@@ -142,16 +154,13 @@ define( function( require ) {
       context.strokeStyle = Color.DARK_GRAY.computeCSS();
       context.stroke();
 
-      for ( i = 0; i < this.model.strand1Segments.length; i++ ) {
-        strand1Segment = this.model.strand1Segments[ i ];
-        strand2Segment = this.model.strand2Segments[ i ];
-        if ( i % 2 === 0 ) {
-          this.drawCurve( context, strand1Segment, STRAND_1_COLOR );
-        }
-        else {
-          this.drawCurve( context, strand2Segment, STRAND_2_COLOR );
-        }
-      }
+      this.drawCurve( context, strand1ArrayFront, STRAND_1_COLOR );
+      this.drawCurve( context, strand2ArrayFront, STRAND_2_COLOR );
+
+      strand1ArrayBehind.length = 0;
+      strand2ArrayBehind.length = 0;
+      strand1ArrayFront.length = 0;
+      strand2ArrayFront.length = 0;
     },
 
     /**
