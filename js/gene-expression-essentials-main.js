392df1429870a66f85460c9710478cb577e6d0ac
zepumph <michael.kauzmann@colorado.edu>
2017-08-15 16:39:03 -0800

    remove unnecessary newlines or replace with <br>, https://github.com/phetsims/joist/issues/435

--- js/gene-expression-essentials-main.js
+++ js/gene-expression-essentials-main.js
@@ -20,8 +20,8 @@ define( function( require ) {
 
   var simOptions = {
     credits: {
-      softwareDevelopment: 'Sharfudeen Ashraf, John Blanco, George Emanuel,\nAadish Gupta',
-      team: 'John Blanco, Steele Dalton, Mike Klymkowsky, Amanda McGarry,\nAriel Paul, Katherine Perkins, Tom Perkins',
+      softwareDevelopment: 'Sharfudeen Ashraf, John Blanco, George Emanuel, Aadish Gupta',
+      team: 'John Blanco, Steele Dalton, Mike Klymkowsky, Amanda McGarry, Ariel Paul, Katherine Perkins, Tom Perkins',
       qualityAssurance: 'Steele Dalton, Bryce Griebenow',
       graphicArts: '',
       thanks: ''
