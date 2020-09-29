306f1a852723b4beb1e552f73631d25aa8ed6fd8
pixelzoom <cmalley@pixelzoom.com>
2020-09-28 22:53:47 -0600

    delete this.expandedProperty, convert to ES6, https://github.com/phetsims/gene-expression-essentials/issues/134

--- js/multiple-cells/view/ControlPanelNode.js
+++ js/multiple-cells/view/ControlPanelNode.js
@@ -8,8 +8,6 @@
  * @author Aadish Gupta
  */
 
-import Property from '../../../../axon/js/Property.js';
-import inherit from '../../../../phet-core/js/inherit.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
 import Text from '../../../../scenery/js/nodes/Text.js';
 import VBox from '../../../../scenery/js/nodes/VBox.js';
@@ -19,67 +17,66 @@ import GEEConstants from '../../common/GEEConstants.js';
 import ControllerNode from '../../common/view/ControllerNode.js';
 import geneExpressionEssentials from '../../geneExpressionEssentials.js';
 
-/**
- * @param {String} title
- * @param {Array<Object>}controllers
- * @constructor
- */
-function ControlPanelNode( title, controllers ) {
+class ControlPanelNode extends AccordionBox {
 
-  const controllerNodes = [];
+  /**
+   * @param {String} title
+   * @param {Array<Object>}controllers
+   */
+  constructor( title, controllers ) {
 
-  for ( let i = 0; i < controllers.length; i++ ) {
-    const controller = controllers[ i ];
-    const label = new Text( controller.label, {
-      font: new PhetFont( { size: 13 } ),
-      maxWidth: 200
-    } );
-    const controllerNode = new ControllerNode(
-      controller.controlProperty,
-      controller.minValue,
-      controller.maxValue,
-      controller.minLabel,
-      controller.maxLabel,
-      {
-        logScale: controller.logScale
-      }
-    );
-    controllerNodes[ i ] = new VBox( {
+    const controllerNodes = [];
+
+    for ( let i = 0; i < controllers.length; i++ ) {
+      const controller = controllers[ i ];
+      const label = new Text( controller.label, {
+        font: new PhetFont( { size: 13 } ),
+        maxWidth: 200
+      } );
+      const controllerNode = new ControllerNode(
+        controller.controlProperty,
+        controller.minValue,
+        controller.maxValue,
+        controller.minLabel,
+        controller.maxLabel,
+        {
+          logScale: controller.logScale
+        }
+      );
+      controllerNodes[ i ] = new VBox( {
+        spacing: 5,
+        children: [
+          label,
+          controllerNode
+        ]
+      } );
+    }
+
+    const contentNode = new VBox( {
       spacing: 5,
-      children: [
-        label,
-        controllerNode
-      ]
+      children: controllerNodes
+    } );
+
+    super( contentNode, {
+      titleNode: new Text( title, {
+        font: new PhetFont( { size: 16, weight: 'bold' } ),
+        maxWidth: 200
+      } ),
+      cornerRadius: GEEConstants.CORNER_RADIUS,
+      titleAlignX: 'left',
+      contentAlign: 'center',
+      fill: new Color( 220, 236, 255 ),
+      buttonXMargin: 6,
+      buttonYMargin: 6,
+      contentYMargin: 8,
+      expandCollapseButtonOptions: {
+        touchAreaXDilation: 8,
+        touchAreaYDilation: 8
+      },
+      minWidth: 200
     } );
   }
-
-  const contentNode = new VBox( {
-    spacing: 5,
-    children: controllerNodes
-  } );
-
-  this.expandedProperty = new Property( false );
-  AccordionBox.call( this, contentNode, {
-    titleNode: new Text( title, {
-      font: new PhetFont( { size: 16, weight: 'bold' } ),
-      maxWidth: 200
-    } ),
-    cornerRadius: GEEConstants.CORNER_RADIUS,
-    titleAlignX: 'left',
-    contentAlign: 'center',
-    fill: new Color( 220, 236, 255 ),
-    buttonXMargin: 6,
-    buttonYMargin: 6,
-    contentYMargin: 8,
-    expandedProperty: this.expandedProperty,
-    expandCollapseButtonOptions: {
-      touchAreaXDilation: 8,
-      touchAreaYDilation: 8
-    },
-    minWidth: 200
-  } );
 }
 
 geneExpressionEssentials.register( 'ControlPanelNode', ControlPanelNode );
-inherit( AccordionBox, ControlPanelNode );
 export default ControlPanelNode;
