ba65e28dba45ca2a71284110bc54cb4e8845bf9c
jbphet <john.blanco@colorado.edu>
2020-10-09 15:16:25 -0600

    converted to ES6, see https://github.com/phetsims/tasks/issues/1049

--- js/manual-gene-expression/view/BiomoleculeToolboxNode.js
+++ js/manual-gene-expression/view/BiomoleculeToolboxNode.js
@@ -1,15 +1,14 @@
 // Copyright 2015-2020, University of Colorado Boulder
 
 /**
- * This class defines the box on the user interface from which the user can extract various biomolecules and put them
- * into action within the cell.
+ * BiomoleculeToolboxNode is a Scenery node that portrays a box from which the user can extract various biomolecules and
+ * put them into action within the cell.
  *
  * @author Sharfudeen Ashraf
  * @author John Blanco
  * @author Aadish Gupta
  */
 
-import inherit from '../../../../phet-core/js/inherit.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
 import HBox from '../../../../scenery/js/nodes/HBox.js';
 import Node from '../../../../scenery/js/nodes/Node.js';
@@ -20,8 +19,8 @@ import VBox from '../../../../scenery/js/nodes/VBox.js';
 import Color from '../../../../scenery/js/util/Color.js';
 import Panel from '../../../../sun/js/Panel.js';
 import GEEConstants from '../../common/GEEConstants.js';
-import geneExpressionEssentialsStrings from '../../geneExpressionEssentialsStrings.js';
 import geneExpressionEssentials from '../../geneExpressionEssentials.js';
+import geneExpressionEssentialsStrings from '../../geneExpressionEssentialsStrings.js';
 import MessengerRnaDestroyerCreatorNode from './MessengerRnaDestroyerCreatorNode.js';
 import RibosomeCreatorNode from './RibosomeCreatorNode.js';
 import RnaPolymeraseCreatorNode from './RnaPolymeraseCreatorNode.js';
@@ -37,176 +36,184 @@ const positiveTranscriptionFactorHtmlString = geneExpressionEssentialsStrings.po
 const ribosomeString = geneExpressionEssentialsStrings.ribosome;
 const rnaPolymeraseString = geneExpressionEssentialsStrings.rnaPolymerase;
 
-/**
- * Convenience function for creating row labels.
- */
-function RowLabel( text ) {
-  return new RichText( text, {
-    font: new PhetFont( { size: 15 } ),
-    maxWidth: 150,
-    align: 'center'
-  } );
-}
-
-/**
- * @param {ManualGeneExpressionModel} model
- * @param {ManualGeneExpressionScreenView} canvas
- * @param {ModelViewTransform2} modelViewTransform
- * @param {Gene} gene
- * @constructor
- */
-function BiomoleculeToolboxNode( model, canvas, modelViewTransform, gene ) {
-
-  const self = this;
-  this.biomoleculeCreatorNodeList = [];
-
-  //TODO These 3 fields are @private and not used. Can they be deleted?
-  this.model = model; //@private
-  this.canvas = canvas; //@private
-  this.modelViewTransform = modelViewTransform; //@private
-
-  // Add the title.
-  const toolboxTitleNode = new Text( biomoleculeToolboxString, {
-    font: TITLE_FONT,
-    maxWidth: 200
-  } );
-
-  const positiveTranscriptionFactorLabel = new RowLabel( positiveTranscriptionFactorHtmlString );
-  const positiveTranscriptionFactorLabelWidth = positiveTranscriptionFactorLabel.width;
-  const rnaPolymeraseLabel = new RowLabel( rnaPolymeraseString );
-  const rnaPolymeraseLabelWidth = rnaPolymeraseLabel.width;
-  const ribosomeLabel = new RowLabel( ribosomeString );
-  const ribosomeLabelWidth = ribosomeLabel.width;
-  const mrnaDestroyerLabel = new RowLabel( mRnaDestroyerString );
-  const mrnaDestroyerLabelWidth = mrnaDestroyerLabel.width;
-  const negativeTranscriptionFactorLabel = new RowLabel( negativeTranscriptionFactorHtmlString );
-  const negativeTranscriptionFactorLabelWidth = negativeTranscriptionFactorLabel.width;
-
-  const maxWidth = _.max( [
-    positiveTranscriptionFactorLabelWidth,
-    rnaPolymeraseLabelWidth,
-    ribosomeLabelWidth,
-    mrnaDestroyerLabelWidth,
-    negativeTranscriptionFactorLabelWidth
-  ] );
-
-  // Transcription factor(s).
-  const transcriptionFactors = gene.getTranscriptionFactorConfigs();
-  const positiveTranscriptBoxNodes = [];
-  const negativeTranscriptBoxNodes = [];
-  transcriptionFactors.forEach( function( tfConfig ) {
-    const creatorNode = self.addCreatorNode( new TranscriptionFactorCreatorNode( self, tfConfig ) );
-    if ( tfConfig.isPositive ) {
-      const positiveTranscriptionBox = new HBox( {
-        children: [
-          positiveTranscriptionFactorLabel,
-          new Spacer( maxWidth - positiveTranscriptionFactorLabelWidth, 0 ),
-          creatorNode
-        ],
-        spacing: 10
-      } );
-      positiveTranscriptBoxNodes.push( positiveTranscriptionBox );
-    }
-    else {
-      const negativeTranscriptionBox = new HBox( {
-        children: [
-          negativeTranscriptionFactorLabel,
-          new Spacer( maxWidth - negativeTranscriptionFactorLabelWidth, 0 ),
-          creatorNode
-        ],
-        spacing: 10
-      } );
-      negativeTranscriptBoxNodes.push( negativeTranscriptionBox );
-    }
-  } );
-
-  // Polymerase.
-  const polymeraseBox = new HBox( {
-    children: [
-      rnaPolymeraseLabel,
-      new Spacer( maxWidth - rnaPolymeraseLabelWidth, 0 ),
-      this.addCreatorNode( new RnaPolymeraseCreatorNode( this ) ),
-      this.addCreatorNode( new RnaPolymeraseCreatorNode( this ) )
-    ],
-    spacing: 10
-  } );
-
-  // Ribosomes.
-  const ribosomeBox = new HBox( {
-    children: [
-      ribosomeLabel,
-      new Spacer( maxWidth - ribosomeLabelWidth, 0 ),
-      this.addCreatorNode( new RibosomeCreatorNode( this ) ),
-      this.addCreatorNode( new RibosomeCreatorNode( this ) )
-    ],
-    spacing: 10
-  } );
-
-  // mRNA destroyer.
-  const mRnaDestroyerBox = new HBox( {
-    children: [
-      mrnaDestroyerLabel,
-      new Spacer( maxWidth - mrnaDestroyerLabelWidth, 0 ),
-      this.addCreatorNode( new MessengerRnaDestroyerCreatorNode( this ) ),
-      this.addCreatorNode( new MessengerRnaDestroyerCreatorNode( this ) )
-    ],
-    spacing: 10
-  } );
-
-  let childrenNodesArray = [];
-  childrenNodesArray = childrenNodesArray.concat( positiveTranscriptBoxNodes );
-  childrenNodesArray.push( polymeraseBox );
-  childrenNodesArray.push( ribosomeBox );
-  childrenNodesArray.push( mRnaDestroyerBox );
-  childrenNodesArray = childrenNodesArray.concat( negativeTranscriptBoxNodes );
-  // Create the content of this control panel.
-  const contentNode = new Node();
-  contentNode.addChild( toolboxTitleNode );
-  const childrenNode = new VBox( {
-    children: childrenNodesArray,
-    spacing: 10,
-    align: 'left'
-  } );
-  contentNode.addChild( childrenNode );
-  childrenNode.top = toolboxTitleNode.bottom + 10;
-  toolboxTitleNode.centerX = childrenNode.centerX;
-
-  Panel.call( this, contentNode, {
-    cornerRadius: GEEConstants.CORNER_RADIUS,
-    xMargin: 10,
-    yMargin: 10,
-    fill: new Color( 250, 250, 250 ),
-    lineWidth: 1,
-    align: 'center',
-    resize: false
-  } );
-}
-
-geneExpressionEssentials.register( 'BiomoleculeToolboxNode', BiomoleculeToolboxNode );
-
-inherit( Panel, BiomoleculeToolboxNode, {
+class BiomoleculeToolboxNode extends Node {
 
   /**
-   * @public reset the toolbox
+   * @param {ManualGeneExpressionModel} model
+   * @param {ManualGeneExpressionScreenView} canvas
+   * @param {ModelViewTransform2} modelViewTransform
+   * @param {Gene} gene
    */
-  reset: function() {
+  constructor( model, canvas, modelViewTransform, gene ) {
+
+    super();
+
+    // @public {ManualGeneExpressionModel} - used by the creator nodes to add new biomolecules to the model
+    this.model = model;
+
+    // @public {ManualGeneExpressionScreenView} - used by the creator nodes to add new biomolecule nodes to the view
+    this.canvas = canvas;
+
+    // @public {ModelViewTransform2} - used by the creator nodes to add new biomolecule nodes to the view
+    this.modelViewTransform = modelViewTransform;
+
+    // @private
+    this.biomoleculeCreatorNodeList = [];
+
+    // title
+    const toolboxTitleNode = new Text( biomoleculeToolboxString, {
+      font: TITLE_FONT,
+      maxWidth: 200
+    } );
+
+    // labels for the rows
+    const positiveTranscriptionFactorLabel = BiomoleculeToolboxNode.createRowLabel( positiveTranscriptionFactorHtmlString );
+    const positiveTranscriptionFactorLabelWidth = positiveTranscriptionFactorLabel.width;
+    const rnaPolymeraseLabel = BiomoleculeToolboxNode.createRowLabel( rnaPolymeraseString );
+    const rnaPolymeraseLabelWidth = rnaPolymeraseLabel.width;
+    const ribosomeLabel = BiomoleculeToolboxNode.createRowLabel( ribosomeString );
+    const ribosomeLabelWidth = ribosomeLabel.width;
+    const mrnaDestroyerLabel = BiomoleculeToolboxNode.createRowLabel( mRnaDestroyerString );
+    const mrnaDestroyerLabelWidth = mrnaDestroyerLabel.width;
+    const negativeTranscriptionFactorLabel = BiomoleculeToolboxNode.createRowLabel( negativeTranscriptionFactorHtmlString );
+    const negativeTranscriptionFactorLabelWidth = negativeTranscriptionFactorLabel.width;
+
+    const maxWidth = _.max( [
+      positiveTranscriptionFactorLabelWidth,
+      rnaPolymeraseLabelWidth,
+      ribosomeLabelWidth,
+      mrnaDestroyerLabelWidth,
+      negativeTranscriptionFactorLabelWidth
+    ] );
+
+    // transcription factor(s)
+    const transcriptionFactors = gene.getTranscriptionFactorConfigs();
+    const positiveTranscriptBoxNodes = [];
+    const negativeTranscriptBoxNodes = [];
+    transcriptionFactors.forEach( tfConfig => {
+      const creatorNode = this.addCreatorNode( new TranscriptionFactorCreatorNode( this, tfConfig ) );
+      if ( tfConfig.isPositive ) {
+        const positiveTranscriptionBox = new HBox( {
+          children: [
+            positiveTranscriptionFactorLabel,
+            new Spacer( maxWidth - positiveTranscriptionFactorLabelWidth, 0 ),
+            creatorNode
+          ],
+          spacing: 10
+        } );
+        positiveTranscriptBoxNodes.push( positiveTranscriptionBox );
+      }
+      else {
+        const negativeTranscriptionBox = new HBox( {
+          children: [
+            negativeTranscriptionFactorLabel,
+            new Spacer( maxWidth - negativeTranscriptionFactorLabelWidth, 0 ),
+            creatorNode
+          ],
+          spacing: 10
+        } );
+        negativeTranscriptBoxNodes.push( negativeTranscriptionBox );
+      }
+    } );
+
+    // polymerase
+    const polymeraseBox = new HBox( {
+      children: [
+        rnaPolymeraseLabel,
+        new Spacer( maxWidth - rnaPolymeraseLabelWidth, 0 ),
+        this.addCreatorNode( new RnaPolymeraseCreatorNode( this ) ),
+        this.addCreatorNode( new RnaPolymeraseCreatorNode( this ) )
+      ],
+      spacing: 10
+    } );
+
+    // ribosomes
+    const ribosomeBox = new HBox( {
+      children: [
+        ribosomeLabel,
+        new Spacer( maxWidth - ribosomeLabelWidth, 0 ),
+        this.addCreatorNode( new RibosomeCreatorNode( this ) ),
+        this.addCreatorNode( new RibosomeCreatorNode( this ) )
+      ],
+      spacing: 10
+    } );
+
+    // mRNA destroyer
+    const mRnaDestroyerBox = new HBox( {
+      children: [
+        mrnaDestroyerLabel,
+        new Spacer( maxWidth - mrnaDestroyerLabelWidth, 0 ),
+        this.addCreatorNode( new MessengerRnaDestroyerCreatorNode( this ) ),
+        this.addCreatorNode( new MessengerRnaDestroyerCreatorNode( this ) )
+      ],
+      spacing: 10
+    } );
+
+    let childrenNodesArray = [];
+    childrenNodesArray = childrenNodesArray.concat( positiveTranscriptBoxNodes );
+    childrenNodesArray.push( polymeraseBox );
+    childrenNodesArray.push( ribosomeBox );
+    childrenNodesArray.push( mRnaDestroyerBox );
+    childrenNodesArray = childrenNodesArray.concat( negativeTranscriptBoxNodes );
+
+    // create the content of the panel
+    const contentNode = new Node();
+    contentNode.addChild( toolboxTitleNode );
+    const childrenNode = new VBox( {
+      children: childrenNodesArray,
+      spacing: 10,
+      align: 'left'
+    } );
+    contentNode.addChild( childrenNode );
+    childrenNode.top = toolboxTitleNode.bottom + 10;
+    toolboxTitleNode.centerX = childrenNode.centerX;
+
+    // add the panel
+    this.addChild( new Panel( contentNode, {
+      cornerRadius: GEEConstants.CORNER_RADIUS,
+      xMargin: 10,
+      yMargin: 10,
+      fill: new Color( 250, 250, 250 ),
+      lineWidth: 1,
+      align: 'center',
+      resize: false
+    } ) );
+  }
+
+  /**
+   * convenience function to push a creator node on to the creator node list and return a reference to it
+   * @param {BiomoleculeCreatorNode} creatorNode
+   * @returns {BiomoleculeCreatorNode}
+   * @private
+   */
+  addCreatorNode( creatorNode ) {
+    this.biomoleculeCreatorNodeList.push( creatorNode );
+    return creatorNode;
+  }
+
+  /**
+   * reset the toolbox by resetting each of the biomolecule creator nodes
+   * @public
+   */
+  reset() {
     const bioMoleculeCreatorNodeLength = this.biomoleculeCreatorNodeList.length;
     for ( let i = 0; i < bioMoleculeCreatorNodeLength; i++ ) {
       this.biomoleculeCreatorNodeList[ i ].reset();
     }
-  },
+  }
 
   /**
-   * Convenience function for making it easy to create a biomolecule creator node and add it to the content panel at
-   * the same time.
-   * @param {BiomoleculeCreatorNode} biomoleculeCreatorNode
-   * @returns BiomoleculeCreatorNode
+   * convenience function for creating row labels
    * @private
    */
-  addCreatorNode: function( biomoleculeCreatorNode ) {
-    this.biomoleculeCreatorNodeList.push( biomoleculeCreatorNode );
-    return biomoleculeCreatorNode;
+  static createRowLabel( text ) {
+    return new RichText( text, {
+      font: new PhetFont( { size: 15 } ),
+      maxWidth: 150,
+      align: 'center'
+    } );
   }
-} );
+}
 
+geneExpressionEssentials.register( 'BiomoleculeToolboxNode', BiomoleculeToolboxNode );
 export default BiomoleculeToolboxNode;
