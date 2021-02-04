6e9615448f1b8e8a41f7c401565e08acbb538b41
zepumph <michael.kauzmann@colorado.edu>
2021-02-03 16:32:45 -0900

    add period to some parameterized type docs. Convert `{Function.<` to `{function():` notation, https://github.com/phetsims/chipper/issues/1005

--- js/common/model/DnaMolecule.js
+++ js/common/model/DnaMolecule.js
@@ -482,9 +482,9 @@ class DnaMolecule {
    * Consider a proposal from a biomolecule. This is the generic version that avoids duplicated code.
    * @param {MobileBiomolecule} biomolecule
    * @param {number} maxAttachDistance
-   * @param {Function.<Number>} getAttachSiteForBasePair
-   * @param {Function.<Gene>} isOkayToAttach
-   * @param {Function.<Gene>} getAttachmentSite
+   * @param {function(number):AttachmentSite} getAttachSiteForBasePair
+   * @param {function(Gene):boolean} isOkayToAttach
+   * @param {function(Gene):AttachmentSite} getAttachmentSite
    * @returns {AttachmentSite}
    * @private
    */
