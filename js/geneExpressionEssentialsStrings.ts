84497de809d20f045cff148843dc59d542ad0772
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:25 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/geneExpressionEssentialsStrings.ts
@@ -0,0 +1,64 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import geneExpressionEssentials from './geneExpressionEssentials.js';
+
+type StringsType = {
+  'gene-expression-essentials': {
+    'title': string
+  },
+  'screen': {
+    'expression': string,
+    'mRna': string,
+    'multipleCells': string
+  },
+  'low': string,
+  'lots': string,
+  'regulatoryRegion': string,
+  'negativeTranscriptionFactor': string,
+  'ribosome': string,
+  'affinities': string,
+  'biomoleculeToolbox': string,
+  'one': string,
+  'concentration': string,
+  'protein': string,
+  'slow': string,
+  'cells': string,
+  'yourProteinCollection': string,
+  'previousGene': string,
+  'positiveTranscriptionFactor': string,
+  'quotedMRna': string,
+  'mRnaDestroyer': string,
+  'many': string,
+  'none': string,
+  'averageProteinLevel': string,
+  'averageProteinLevelVsTime': string,
+  'time': string,
+  'transcribedRegion': string,
+  'nextGene': string,
+  'negativeTranscriptionFactorHtml': string,
+  'positiveTranscriptionFactorHtml': string,
+  'gene': string,
+  'affinity': string,
+  'imageCaption': string,
+  'imageCaptionNote': string,
+  'rnaPolymerase': string,
+  'proteinCountCaptionPart1': string,
+  'proteinCountCaptionPart2': string,
+  'showRealCells': string,
+  'high': string,
+  'degradation': string,
+  'collectionComplete': string,
+  'fast': string,
+  'polymerase': string
+};
+
+const geneExpressionEssentialsStrings = getStringModule( 'GENE_EXPRESSION_ESSENTIALS' ) as StringsType;
+
+geneExpressionEssentials.register( 'geneExpressionEssentialsStrings', geneExpressionEssentialsStrings );
+
+export default geneExpressionEssentialsStrings;
