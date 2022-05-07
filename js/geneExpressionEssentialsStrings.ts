1066a40c1b9932167a5fd79052eba6c752ab138b
Sam Reid <reids@colorado.edu>
2022-05-07 09:33:12 -0600

    Use semicolons to terminate fields in type StringType, see https://github.com/phetsims/chipper/issues/1208

--- js/geneExpressionEssentialsStrings.ts
+++ js/geneExpressionEssentialsStrings.ts
@@ -9,52 +9,52 @@ import geneExpressionEssentials from './geneExpressionEssentials.js';
 
 type StringsType = {
   'gene-expression-essentials': {
-    'title': string
-  },
+    'title': string;
+  };
   'screen': {
-    'expression': string,
-    'mRna': string,
-    'multipleCells': string
-  },
-  'low': string,
-  'lots': string,
-  'regulatoryRegion': string,
-  'negativeTranscriptionFactor': string,
-  'ribosome': string,
-  'affinities': string,
-  'biomoleculeToolbox': string,
-  'one': string,
-  'concentration': string,
-  'protein': string,
-  'slow': string,
-  'cells': string,
-  'yourProteinCollection': string,
-  'previousGene': string,
-  'positiveTranscriptionFactor': string,
-  'quotedMRna': string,
-  'mRnaDestroyer': string,
-  'many': string,
-  'none': string,
-  'averageProteinLevel': string,
-  'averageProteinLevelVsTime': string,
-  'time': string,
-  'transcribedRegion': string,
-  'nextGene': string,
-  'negativeTranscriptionFactorHtml': string,
-  'positiveTranscriptionFactorHtml': string,
-  'gene': string,
-  'affinity': string,
-  'imageCaption': string,
-  'imageCaptionNote': string,
-  'rnaPolymerase': string,
-  'proteinCountCaptionPart1': string,
-  'proteinCountCaptionPart2': string,
-  'showRealCells': string,
-  'high': string,
-  'degradation': string,
-  'collectionComplete': string,
-  'fast': string,
-  'polymerase': string
+    'expression': string;
+    'mRna': string;
+    'multipleCells': string;
+  };
+  'low': string;
+  'lots': string;
+  'regulatoryRegion': string;
+  'negativeTranscriptionFactor': string;
+  'ribosome': string;
+  'affinities': string;
+  'biomoleculeToolbox': string;
+  'one': string;
+  'concentration': string;
+  'protein': string;
+  'slow': string;
+  'cells': string;
+  'yourProteinCollection': string;
+  'previousGene': string;
+  'positiveTranscriptionFactor': string;
+  'quotedMRna': string;
+  'mRnaDestroyer': string;
+  'many': string;
+  'none': string;
+  'averageProteinLevel': string;
+  'averageProteinLevelVsTime': string;
+  'time': string;
+  'transcribedRegion': string;
+  'nextGene': string;
+  'negativeTranscriptionFactorHtml': string;
+  'positiveTranscriptionFactorHtml': string;
+  'gene': string;
+  'affinity': string;
+  'imageCaption': string;
+  'imageCaptionNote': string;
+  'rnaPolymerase': string;
+  'proteinCountCaptionPart1': string;
+  'proteinCountCaptionPart2': string;
+  'showRealCells': string;
+  'high': string;
+  'degradation': string;
+  'collectionComplete': string;
+  'fast': string;
+  'polymerase': string;
 };
 
 const geneExpressionEssentialsStrings = getStringModule( 'GENE_EXPRESSION_ESSENTIALS' ) as StringsType;
