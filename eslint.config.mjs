d61d183a2608676c8ac9da5ca6d293400c2b4130
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:09:13 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for gene-expression-essentials.
+ * ESLint configuration for gene-expression-essentials.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
